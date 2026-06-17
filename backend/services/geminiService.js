2f667e74f5bb1dc901f71f170e9306dd879af6ea
thefahadsameer <fahad0000072015@gmail.com>
2026-06-17 17:04:08 +0530

    Changes in Chatbot 3

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(
 );
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash"
+  model: "gemini-1.5-flash"
 });
 
 async function askGemini(prompt) {
